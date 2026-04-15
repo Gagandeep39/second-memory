8d47894959c427f0e4bf37c52143ef799d71ddec
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2026-04-15 14:02:56 -0400

    #3 Fixed inconsistent spacing

--- app/src/main/java/com/secondmemory/ui/screen/dailyview/DailyViewScreen.kt
+++ app/src/main/java/com/secondmemory/ui/screen/dailyview/DailyViewScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
@@ -289,8 +290,7 @@ fun DailyViewScreen(
         floatingActionButton = {
             Column(
                 horizontalAlignment = Alignment.End,
-                verticalArrangement = Arrangement.spacedBy(16.dp),
-                modifier = Modifier.padding(8.dp)
+                verticalArrangement = Arrangement.spacedBy(16.dp)
             ) {
                 FloatingActionButton(
                     onClick = {
